df6cbdaf9be50183f3be29bb4c3d70b599156777
Teresa Heidt <teresaheidt@hotmail.com>
2020-11-23 18:14:09 -0600

    new landing page

---
+++ client/src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+    {
+        title: 'Sign Up',
+        description:
+        'And change your life and your fortune forever.',
+        imageUrl: process.env.PUBLIC_URL + '/assets/snake_skin.jpg',
+        time: 1500
+    },
+    {
+        title: 'Login In',
+        description:
+        'To recieve the best stock picks of the day.',
+        imageUrl: process.env.PUBLIC_URL + '/assets/snake_skin.jpg',
+        time: 1500
+    },
+];
+
+export default places;
