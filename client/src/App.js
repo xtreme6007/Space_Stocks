d11a5e1b04eece0a37f8a36fe20b9fc324b13cda
xtreme6007 <prestonnichols96@gmail.com>
2020-11-29 20:07:30 -0600

    fixed mess up

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { makeStyles, CssBaseline } from '@material-ui/core';
 import Header from './components/Header';
 import PlaceToVisit from './components/PlaceToVisit';
-import APItest from './components/APItest';
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,10 +22,9 @@ export default function() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      {/* <CssBaseline />
+      <CssBaseline />
       <Header />
-      <PlaceToVisit /> */}
-      <APItest />
+      <PlaceToVisit />
     </div>
   )
 };
