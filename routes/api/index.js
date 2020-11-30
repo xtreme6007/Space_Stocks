b291a27c866f3383fa178fef5e3a69d39e0ea661
xtreme6007 <prestonnichols96@gmail.com>
2020-11-29 20:49:04 -0600

    PLEASE REVIEW IDK WHAT IM DOING but i think i set up the routes and the controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,18 @@
 const router = require("express").Router();
+
+const router = require("express").Router();
+const booksController = require("../../controllers/StockController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  
+
+module.exports = router;
